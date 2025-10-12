1c0e9523cd2635cbb27201af65c4376bee3a41cd
ALEGUMEN <aleguamen_@hotmail.com>
2025-10-11 22:46:33 -0600

    decimo commit

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
+const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views/pug')); 
-app.set('view engine', 'pug');
+// add this CORS header
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
-fccTesting(app); // For FCC testing purposes
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+fccTesting(app); // For fCC testing purposes
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('index'); // <- exacto: sólo 'index'
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });

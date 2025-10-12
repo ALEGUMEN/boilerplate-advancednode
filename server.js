5c7212de128ff2d4cab317b7c7a487458eba2a02
ALEGUMEN <aleguamen_@hotmail.com>
2025-10-11 22:19:36 -0600

    fourth commit

--- server.js
+++ server.js
@@ -6,16 +6,15 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 app.set('view engine', 'pug');
-app.set('views', './views/pug');
+app.route('/').get((req, res) => {
+res.render('pug/index');
+});
 
 fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('index');
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

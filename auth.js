c854b8105ebb4117e350296925ae483f29195164
ALEGUMEN <aleguamen_@hotmail.com>
2025-10-15 12:24:20 -0600

    auth fix

--- auth.js
+++ auth.js
@@ -1,60 +1,48 @@
-'use strict';
-require('dotenv').config();
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const GitHubStrategy = require('passport-github').Strategy;
-const bcrypt = require('bcrypt');
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const GitHubStrategy = require('passport-github').Strategy;
+const GITHUB_CLIENT_SECRET = process.env['GITHUB_CLIENT_SECRET'];
+const GITHUB_CLIENT_ID = process.env['GITHUB_CLIENT_ID'];
 
-module.exports = function(app, myDataBase) {
-
-  // ----------------------
-  // SERIALIZE / DESERIALIZE
-  // ----------------------
-  passport.serializeUser((user, done) => done(null, user._id));
+module.exports = function (app, myDataBase) {
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      if (err) return done(err, null);
+      if (err) return console.error(err);
       done(null, doc);
     });
   });
-
-  // ----------------------
-  // LOCAL STRATEGY
-  // ----------------------
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username }, (err, user) => {
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (!bcrypt.compareSync(password, user.password)) return done(null, false);
-      return done(null, user);
-    });
-  }));
-
-  // ----------------------
-  // GITHUB STRATEGY
-  // ----------------------
+  
   passport.use(new GitHubStrategy({
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.GITHUB_CALLBACK_URL || "http://localhost:3000/auth/github/callback"
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: 'https://boilerplate-advancednode-s02l.onrender.com/auth/github/callback'
     },
-    function(accessToken, refreshToken, profile, cb) {
-      console.log('GitHub profile:', profile);
-      myDataBase.findOne({ githubId: profile.id }, (err, user) => {
-        if (err) return cb(err);
-        if (!user) {
-          myDataBase.insertOne({
-            githubId: profile.id,
-            username: profile.username
-          }, (err, doc) => {
-            if (err) return cb(err);
-            return cb(null, doc.ops[0]);
-          });
-        } else {
-          return cb(null, user);
-        }
-      });
+     function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      //Database logic here with callback containing our user object
+      myDataBase.findOneAndUpdate( { id: profile.id }, 
+      {
+        $setOnInsert: {
+          id: profile.id,
+          name: profile.displayName || 'John Doe',
+          photo: profile.photos[0].value || '',
+          email: Array.isArray(profile.emails)
+            ? profile.emails[0].value
+            : 'No public email',
+          created_on: new Date(),
+          provider: profile.provider || ''
+        },
+        $set: { last_login: new Date() },
+        $inc: { login_count: 1 }
+      },
+      { upsert: true, new: true },
+      (err, doc) => { return cb(null, doc.value); }
+      );
     }
   ));
-};
+}

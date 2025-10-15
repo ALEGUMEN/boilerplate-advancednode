565ca59c0bc55f1d674e2ec2155e54d320cd4078
ALEGUMEN <aleguamen_@hotmail.com>
2025-10-15 15:05:14 -0600

    auth

--- routes.js
+++ routes.js
@@ -4,25 +4,31 @@ const bcrypt = require('bcrypt');
 module.exports = function (app, myDataBase) {
   app.route('/').get((req, res) => {
     // Change the response to render the Pug template
-    res.render('pug', { title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true, showSocialAuth: true });
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true,
+      showRegistration: true,
+      showSocialAuth: true
+    });
   });
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render('pug/profile', { username: req.user.username });
+    res.render(process.cwd() + 'pug/profile', { username: req.user.username });
   });
   app.route('/chat').get(ensureAuthenticated, (req, res) => {
     res.render('pug/chat', { user: req.user });
-  });
+  });  
   app.route('/logout').get((req, res) => {
-    req.logout();
-    res.redirect('/');
-  });
+      req.logout();
+      res.redirect('/');
+  }); 
   app.route('/register').post(
     (req, res, next) => {
       const hash = bcrypt.hashSync(req.body.password, 12);
-      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+      myDataBase.findOne({ username: req.body.username }, function(err, user) {
         if (err) {
           next(err);
         } else if (user) {
@@ -43,16 +49,15 @@ module.exports = function (app, myDataBase) {
       res.redirect('/profile');
     }
   );
-
+    app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found')
+  });
   app.route('/auth/github').get(passport.authenticate('github'));
-  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
-    req.session.user_id = req.user.id;
-    res.redirect('/chat');
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/'}), (req, res) => {
+    req.session.user_id = req.user.id; 
+    res.redirect('/chat'); 
   });
 
-  app.use((req, res, next) => {
-    res.status(404).type('text').send('Not Found');
-  });
 };
 
 function ensureAuthenticated(req, res, next) {
@@ -60,4 +65,4 @@ function ensureAuthenticated(req, res, next) {
     return next();
   }
   res.redirect('/');
-}
+};

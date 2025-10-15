331e390a708018fc054223256d38c505cf2a0665
ALEGUMEN <aleguamen_@hotmail.com>
2025-10-15 15:03:40 -0600

    auth

--- auth.js
+++ auth.js
@@ -1,8 +1,10 @@
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
-const ObjectID = require('mongodb').ObjectID;
 const GitHubStrategy = require('passport-github').Strategy;
+const GITHUB_CLIENT_SECRET = process.env['GITHUB_CLIENT_SECRET'];
+const GITHUB_CLIENT_ID = process.env['GITHUB_CLIENT_ID'];
 
 module.exports = function (app, myDataBase) {
   passport.serializeUser((user, done) => {
@@ -14,49 +16,34 @@ module.exports = function (app, myDataBase) {
       done(null, doc);
     });
   });
-  passport.use(new LocalStrategy(
-    function (username, password, done) {
-      myDataBase.findOne({ username: username }, function (err, user) {
-        console.log('User ' + username + ' attempted to log in.');
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
-        return done(null, user);
-      });
-    }
-  ));
-
+  
   passport.use(new GitHubStrategy({
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'https://boilerplate-advancednode-s02l.onrender.com/auth/github/callback'
-    },
-    function (accessToken, refreshToken, profile, cb) {
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: 'https://boilerplate-advancednode-s02l.onrender.com/auth/github/callback'
+  },
+    function(accessToken, refreshToken, profile, cb) {
       console.log(profile);
-      myDataBase.findAndModify(
-        { id: profile.id },
-        {},
-        {
-          $setOnInsert: {
-            id: profile.id,
-            name: profile.displayName || 'John Doe',
-            photo: profile.photos[0].value || '',
-            email: Array.isArray(profile.emails) ? profile.emails[0].value : 'No public email',
-            created_on: new Date(),
-            provider: profile.provider || ''
-          },
-          $set: {
-            last_login: new Date()
-          },
-          $inc: {
-            login_count: 1
-          }
+      //Database logic here with callback containing our user object
+      myDataBase.findOneAndUpdate( { id: profile.id }, 
+      {
+        $setOnInsert: {
+          id: profile.id,
+          name: profile.displayName || 'John Doe',
+          photo: profile.photos[0].value || '',
+          email: Array.isArray(profile.emails)
+            ? profile.emails[0].value
+            : 'No public email',
+          created_on: new Date(),
+          provider: profile.provider || ''
         },
-        { upsert: true, new: true },
-        (err, doc) => {
-          return cb(null, doc.value);
-        }
+        $set: { last_login: new Date() },
+        $inc: { login_count: 1 }
+      },
+      { upsert: true, new: true },
+      (err, doc) => { return cb(null, doc.value); }
       );
     }
   ));
-};
+}
+ 

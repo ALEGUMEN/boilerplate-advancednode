67122d5a6c88e2a9bbf0705c085c0fc6907c659b
ALEGUMEN <aleguamen_@hotmail.com>
2025-10-15 22:55:39 -0600

    fix

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 });
 
 // View engine
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views', 'pug'));
 app.set('view engine', 'pug');
 
 // Session + Passport
